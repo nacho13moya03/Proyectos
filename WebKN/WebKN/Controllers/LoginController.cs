08823c0aa023497599af1d1cab4f57bba3b4206e
Eduardo Calvo Castillo <ecalvo@crnova.com>
2023-09-22 00:21:24 -0600

    Commit 0

---
+++ WebKN/WebKN/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebKN.Models;
+
+namespace WebKN.Controllers
+{
+    public class LoginController : Controller
+    {
+        Usuario claseUsuario = new Usuario();
+
+        public ActionResult IniciarSesion()
+        { 
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ WebKN/WebKN/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebKN.MvcApplication" Language="C#" %>
