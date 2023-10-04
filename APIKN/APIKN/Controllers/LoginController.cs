716136ee25750659beb3097fc2dc21c3e985efd6
Eduardo Calvo Castillo <ecalvo@crnova.com>
2023-10-03 20:56:13 -0600

    Cambios 3/10

--- APIKN/APIKN/Controllers/LoginController.cs
+++ APIKN/APIKN/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
 ﻿using APIKN.Entities;
+using System.Linq;
 using System.Web.Http;
 
 namespace APIKN.Controllers
@@ -12,18 +13,35 @@ namespace APIKN.Controllers
         {
             using (var context = new BDKNEntities())
             {
-                TUsuario user = new TUsuario();
-                user.Identificacion = entidad.Identificacion;
-                user.Nombre = entidad.Nombre;
-                user.Correo = entidad.Correo;
-                user.Contrasenna = entidad.Contrasenna;
-                user.Estado = entidad.Estado;
-                user.Direccion = entidad.Direccion;
-                
-                context.TUsuario.Add(user);
-                context.SaveChanges();
+                //TUsuario user = new TUsuario();
+                //user.Identificacion = entidad.Identificacion;
+                //user.Nombre = entidad.Nombre;
+                //user.Correo = entidad.Correo;
+                //user.Contrasenna = entidad.Contrasenna;
+                //user.Estado = entidad.Estado;
+                //user.Direccion = entidad.Direccion;
 
+                //context.TUsuario.Add(user);
+                //context.SaveChanges();
+
+                context.RegistrarCuentaSP(entidad.Identificacion, entidad.Nombre, entidad.Correo, entidad.Contrasenna, entidad.Estado, entidad.Direccion);
                 return "Registro realizado correctamente";
+            }
+        }
+
+        [HttpPost]
+        [Route("IniciarSesion")]
+        public IniciarSesionSP_Result IniciarSesion(UsuarioEnt entidad)
+        {
+            using (var context = new BDKNEntities())
+            {
+                //return (from x in context.TUsuario 
+                //             where x.Correo == entidad.Correo
+                //             && x.Contrasenna == entidad.Contrasenna
+                //             && x.Estado == true
+                //             select x).FirstOrDefault();
+
+                return context.IniciarSesionSP(entidad.Correo, entidad.Contrasenna).FirstOrDefault();
             }
         }
 
--- APIKN/APIKN/Model1.Context.cs
+++ APIKN/APIKN/Model1.Context.cs
@@ -12,6 +12,8 @@ namespace APIKN
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
+    using System.Data.Entity.Core.Objects;
+    using System.Linq;
     
     public partial class BDKNEntities : DbContext
     {
@@ -26,5 +28,47 @@ namespace APIKN
         }
     
         public virtual DbSet<TUsuario> TUsuario { get; set; }
+    
+        public virtual int RegistrarCuentaSP(string identificacion, string nombre, string correo, string contrasenna, Nullable<bool> estado, string direccion)
+        {
+            var identificacionParameter = identificacion != null ?
+                new ObjectParameter("Identificacion", identificacion) :
+                new ObjectParameter("Identificacion", typeof(string));
+    
+            var nombreParameter = nombre != null ?
+                new ObjectParameter("Nombre", nombre) :
+                new ObjectParameter("Nombre", typeof(string));
+    
+            var correoParameter = correo != null ?
+                new ObjectParameter("Correo", correo) :
+                new ObjectParameter("Correo", typeof(string));
+    
+            var contrasennaParameter = contrasenna != null ?
+                new ObjectParameter("Contrasenna", contrasenna) :
+                new ObjectParameter("Contrasenna", typeof(string));
+    
+            var estadoParameter = estado.HasValue ?
+                new ObjectParameter("Estado", estado) :
+                new ObjectParameter("Estado", typeof(bool));
+    
+            var direccionParameter = direccion != null ?
+                new ObjectParameter("Direccion", direccion) :
+                new ObjectParameter("Direccion", typeof(string));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("RegistrarCuentaSP", identificacionParameter, nombreParameter, correoParameter, contrasennaParameter, estadoParameter, direccionParameter);
+        }
+    
+        public virtual ObjectResult<IniciarSesionSP_Result> IniciarSesionSP(string correo, string contrasenna)
+        {
+            var correoParameter = correo != null ?
+                new ObjectParameter("Correo", correo) :
+                new ObjectParameter("Correo", typeof(string));
+    
+            var contrasennaParameter = contrasenna != null ?
+                new ObjectParameter("Contrasenna", contrasenna) :
+                new ObjectParameter("Contrasenna", typeof(string));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<IniciarSesionSP_Result>("IniciarSesionSP", correoParameter, contrasennaParameter);
+        }
     }
 }
