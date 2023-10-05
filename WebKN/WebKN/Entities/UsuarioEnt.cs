f3d8613ab952f8465a23656fbac4dd3badc409d8
Eduardo Calvo Castillo <ecalvo@crnova.com>
2023-10-04 19:46:23 -0600

    Update UsuarioEnt.cs

--- WebKN/WebKN/Entities/UsuarioEnt.cs
+++ WebKN/WebKN/Entities/UsuarioEnt.cs
@@ -2,9 +2,12 @@
 {
     public class UsuarioEnt
     {
+        public long ConUsuario { get; set; }
         public string Identificacion { get; set; }
         public string Nombre { get; set; }
         public string Correo { get; set; }
         public string Contrasenna { get; set; }
+        public bool Estado { get; set; }
+        public string Direccion { get; set; }
     }
 }
